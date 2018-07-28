356253e8bdb23da59bf2622d71f85b750a995073
Aristides Perez <aris.a.perez@gmail.com>
2018-07-28 09:51:48 -0400

    Add Pool setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_28_004000) do
+ActiveRecord::Schema.define(version: 2018_07_28_035359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pools", force: :cascade do |t|
+    t.integer "week"
+    t.integer "year"
+    t.text "description"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
