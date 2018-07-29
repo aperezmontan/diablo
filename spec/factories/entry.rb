c2c31df310bce0ca35342f1a89065ca4f5364a5a
Aristides Perez <aris.a.perez@gmail.com>
2018-07-29 02:12:44 -0400

    Fixes

--- spec/factories/entry.rb
+++ spec/factories/entry.rb
@@ -4,7 +4,7 @@
 # This will guess the Entry class
 FactoryBot.define do
   factory :entry do
-    name 'This is an entry'
+    name 'This is an Entry'
     status 0
     teams [0, 1, 2, 3, 4, 5]
     pool
