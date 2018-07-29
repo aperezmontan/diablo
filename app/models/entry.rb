26f52120cd53b1c3751a2b71d7797f1003de598e
Aristides Perez <aris.a.perez@gmail.com>
2018-07-28 22:22:26 -0400

    Make specs pass

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -27,6 +27,8 @@ class Entry < ApplicationRecord
   belongs_to :pool
   belongs_to :user
 
+  delegate :games, to: :pool
+
   # VALIDATIONS
   validates_presence_of :name
   validates_presence_of :status
@@ -48,10 +50,25 @@ class Entry < ApplicationRecord
 
   attr_writer :data
 
+  def all_teams_playing_check
+    teams_hash = games
+                 .inject({}) do |hash, game|
+      hash[game.home_team_before_type_cast] = game.home_team
+      hash.merge!(game.away_team_before_type_cast => game.away_team)
+    end
+
+    teams_not_playing = calc_teams_not_playing(hash: teams_hash)
+    errors.add(:teams, "#{teams_not_playing} have been picked but aren't playing") if teams_not_playing.present?
+  end
+
   def calc_and_save(game:)
     calculate_default_data if data.nil?
     calculate_data(game: game) if game.present?
     save!
+  end
+
+  def calc_teams_not_playing(hash:)
+    teams.select { |team| hash[team].nil? }.map { |team| Game.home_teams.key(team) }
   end
 
   def calculate_data(game:)
@@ -87,6 +104,8 @@ class Entry < ApplicationRecord
   def teams_ok
     too_many_teams_check
     teams_not_unique_check
+    all_teams_playing_check
+    teams_not_playing_each_other_check
     not_enough_teams_check if active?
   end
 
@@ -96,6 +115,15 @@ class Entry < ApplicationRecord
 
   def too_many_teams_check
     errors.add(:teams, 'picked too many') if teams.size > 6
+  end
+
+  def teams_not_playing_each_other_check
+    # rubocop:disable all
+    teams_playing_each_other = games
+      .select{ |game| ([game.home_team_before_type_cast, game.away_team_before_type_cast] - teams).empty? }
+      .map{ |game| [game.home_team, game.away_team] }
+    errors.add(:teams, "#{teams_playing_each_other} are playing each other") if teams_playing_each_other.present?
+    # rubocop:enable all
   end
 
   def winner(game:)
