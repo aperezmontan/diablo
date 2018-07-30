28990e64aa8f3935257e678e2212234fb2b7ea1b
Aristides Perez <aris.a.perez@gmail.com>
2018-07-29 22:59:42 -0400

    Refactor

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -2,30 +2,6 @@
 
 require 'rails_helper'
 
-shared_examples 'an admin user endpoint' do
-  let(:non_admin_user) { create(:user) }
-  let(:result) do
-    return subject unless subject.class == Fixnum # rubocop:disable Lint/UnifiedInteger
-    response
-  end
-
-  before(:each) { sign_out :user }
-
-  context 'when the user is not an admin user' do
-    it 'fails with Unauthorized' do
-      sign_in non_admin_user
-      expect(result).to have_http_status(401)
-    end
-  end
-
-  context 'when the user is an admin user' do
-    it 'succeeds' do
-      sign_in user
-      expect(result).to_not have_http_status(401)
-    end
-  end
-end
-
 describe 'Game', type: :request do
   include Devise::Test::IntegrationHelpers
 
